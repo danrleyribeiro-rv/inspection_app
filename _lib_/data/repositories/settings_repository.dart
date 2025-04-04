d0cc71951ff798365328921b996025c2c80e0996
Danrley Ribeiro <danrley.ribeiro@engenhariarv.com.br>
2025-04-04 16:27:35 -0300

    -

--- _lib_/data/repositories/settings_repository.dart
+++
@@ -1,6 +0,0 @@
-// lib/data/repositories/settings_repository.dart
-abstract class SettingsRepository {
-  Future<Map<String, dynamic>> getSettings();
-  Future<void> saveSettings(Map<String, dynamic> settings);
-  Future<void> clearCache();
-}
