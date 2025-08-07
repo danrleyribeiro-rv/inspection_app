f34cdb5254a0d5e2b9467d950b337a7e28ea7bbd
danrleyribeiro-rv <danrley.ribeiro@engenhariarv.com.br>
2025-08-07 11:20:05 -0300

    Remoção de arquivos desnecessários

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -20,7 +20,6 @@ import share_plus
 import shared_preferences_foundation
 import speech_to_text
 import sqflite_darwin
-import sqlite3_flutter_libs
 import url_launcher_macos
 import video_player_avfoundation
 
@@ -40,7 +39,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
